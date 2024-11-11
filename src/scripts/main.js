65838dc0ad4d837ffedc67b7a3d75efce9419b3f
Eugene Khablenko <eugene.khablenko@gmail.com>
2024-11-11 14:16:08 +0200

    added styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const allAnimated = document.querySelectorAll('.animated');
+
+document.addEventListener('DOMContentLoaded', () => {
+  function checkBlockPosition() {
+    const windowHeight = window.innerHeight;
+
+    allAnimated.forEach((block) => {
+      const blockPosition = block.getBoundingClientRect().top;
+
+      if (blockPosition < windowHeight - 100) {
+        block.style.opacity = 1;
+        block.style.transform = 'translate(0)';
+      }
+    });
+  }
+
+  checkBlockPosition();
+
+  window.addEventListener('scroll', () => {
+    checkBlockPosition();
+  });
+});
